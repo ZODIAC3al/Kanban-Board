191979776105d8064618041b9a170927bca78b84
Ali Maher <121405532+ZODIAC3al@users.noreply.github.com>
2026-01-21 14:50:44 +0200

    remove dummy data

--- js/store.js
+++ js/store.js
@@ -8,51 +8,21 @@ const defaultData = [
     title: "To Do",
     colorClass: "todo",
     count: 8,
-    tasks: [
-      {
-        id: "t1",
-        title: "UI/UX Design in the age of AI",
-        tag: "Important",
-        progress: 0,
-        comments: 11,
-        likes: 187,
-        users: [1, 2],
-      },
-    ],
+    tasks: [],
   },
   {
     id: "col-2",
     title: "In Progress",
     colorClass: "progress",
     count: 2,
-    tasks: [
-      {
-        id: "t4",
-        title: "Machine Learning Progress",
-        tag: "Important",
-        progress: 52,
-        comments: 11,
-        likes: 187,
-        users: [1, 8],
-      },
-    ],
+    tasks: [{}],
   },
   {
     id: "col-3",
     title: "Completed",
     colorClass: "done",
     count: 7,
-    tasks: [
-      {
-        id: "t6",
-        title: "User flow confirmation for fintech",
-        tag: "Important",
-        progress: 100,
-        comments: 11,
-        likes: "2.2K",
-        users: [1, 4],
-      },
-    ],
+    tasks: [{}],
   },
 ];
 
